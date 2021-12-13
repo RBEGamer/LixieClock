8c9b1fb6520967fb177ba3ef902a689d49841991
Marcel Ochsendorf <marcel.ochsendorf@cern.ch>
2021-12-13 18:58:08 +0100

    added mqtt feature

--- src/lixie_esp8266_wifi_extention/lixie_esp8266_wifi_extention.ino
+++ src/lixie_esp8266_wifi_extention/lixie_esp8266_wifi_extention.ino
@@ -14,6 +14,8 @@
 #include <WiFiManager.h>
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
+#include <PubSubClient.h>
+
 
 String time_last = "not synced";
 // CONFIG -------------------------------------
@@ -21,23 +23,34 @@ String time_last = "not synced";
 #define MDNS_NAME "lixieclock" // set hostname
 #define WEBSITE_TITLE "Lixie Clck Confuguration" // name your device
 #define SERIAL_BAUD_RATE 9600
-#define NTP_SEND_TIME_INTERVAL 10 //sende zeit an uhr all x minuten
+#define NTP_SEND_TIME_INTERVAL 5 //sende zeit an uhr all x minuten
 
 #define DEFAULT_NTP_SERVER "pool.ntp.org"
 #define DEFAULT_TIMEZONE 1
-
+#define DEFAULT_MQTT_BROKER "192.168.178.89"
+#define DEFAULT_MQTT_TOPIC "/iot/9770941/humidity"
+#define DEFAULT_MQTT_BROKER_PORT "1883"
 // END CONFIG ---------------------------------
 
 //FILES FOR STORiNG CONFIGURATION DATA
 const char* file_ntp_server = "/file.txt";
 const char* file_timezone = "/timezone.txt";
 const char* file_syncmode = "/syncmode.txt";
+const char* file_mqtt_server = "/mqttbroker.txt";
+const char* file_mqtt_topic = "/mqtttopic.txt";
+const char* file_mqtt_broker_port = "/mqttbrokerport.txt";
+
+
+
 
 
 //VARS
 int sync_mode = 0;
 int timezone = 0;
 String ntp_server_url = "";
+String mqtt_broker_url = "";
+String mqtt_topic = "";
+String mqtt_broker_port = "";
 long long last = 0;
 //NTPSyncEvent_t ntpEvent; // Last triggered event
 
@@ -45,6 +58,9 @@ ESP8266WebServer server(WEBSERVER_PORT);
 
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP,DEFAULT_NTP_SERVER,0,60000);
+
+WiFiClient espClient;
+PubSubClient client(espClient);
 
 
 
@@ -133,7 +149,7 @@ const String pstart = "</head>"
                       "<div class='sidebar'>"
                       "<div class='logo'>"
                       "<span class='title'>LixieCLock</span>"
-                      "<span class='subtitle'>- Backend -</span>"
+                      "<span class='subtitle'>- Configuration -</span>"
                       "</div>"
                       "<div class='menu'>"
                       "<a href='index.html'>Settings</a>"
@@ -169,6 +185,9 @@ void restore_eeprom_values()
     ntp_server_url = read_file(file_ntp_server,DEFAULT_NTP_SERVER);
     timezone = read_file(file_timezone, String(DEFAULT_TIMEZONE)).toInt();
     sync_mode = read_file(file_syncmode, "1").toInt();
+    mqtt_broker_url = read_file(file_mqtt_server,DEFAULT_MQTT_BROKER);
+    mqtt_topic = read_file(file_mqtt_topic,DEFAULT_MQTT_TOPIC);
+    mqtt_broker_port = read_file(file_mqtt_broker_port, String(DEFAULT_MQTT_BROKER_PORT)).toInt();
 }
 
 bool write_file(const char* _file, String _content)
@@ -188,6 +207,69 @@ void save_values_to_eeprom()
     write_file(file_ntp_server, ntp_server_url);
     write_file(file_syncmode, String(sync_mode));
     write_file(file_timezone, String(timezone));
+    write_file(file_timezone, mqtt_broker_url);
+    write_file(file_mqtt_topic, mqtt_topic);
+    write_file(file_mqtt_broker_port, mqtt_broker_port);
+}
+
+
+
+
+void mqtt_callback(char* topic, byte* payload, unsigned int length) {
+
+
+
+  String tmp = "";
+  for (int i = 0; i < length; i++) {
+    tmp += (char)payload[i];
+  }
+  last_error = "GOT MQTT MSG: " + tmp;
+
+
+  //IF MQTT DISPLAY IS ENABLES SEND IT TO CLOCK
+  if(sync_mode == 2 && tmp != ""){
+    float ff = tmp.toFloat();
+    if (isnan(ff)) {
+       send_digits_to_clock(ff);
+    }
+   
+
+  }
+  
+
+}
+
+
+void mqtt_reconnect() {
+  // Loop until we're reconnected
+ if(client.connected() || sync_mode != 2) {return;}
+   
+    // Create a random client ID
+    String clientId = "lixieclock-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str())) {
+      last_error = "MQTT CLICNET CONNCTED";
+
+      
+      client.subscribe(mqtt_topic.c_str());
+    } else {
+      last_error = "MQTT CLCIENT CONECT FAILED WITH" + String(client.state());
+  }
+}
+
+
+
+void send_digits_to_clock(float _f){
+    int whole = (int)_f;
+    int remainder = (_f - whole) * 1000;
+
+    Serial.flush();
+    delay(100);
+    Serial.println("_st_" + String(whole) + "_" + String(remainder) + "_");
+    last_error = "_st_" + String(whole) + "_" + String(remainder) + "_";
+    delay(100);
+ 
 }
 
 
@@ -206,6 +288,14 @@ else if((tmph+timezone)<0){tmph = tmph+timezone+24;}else{
             delay(100);
   }
 
+
+void setup_mqtt_client(){
+   if(sync_mode == 2 && mqtt_broker_url != "" && mqtt_broker_port != ""){
+    client.setServer(mqtt_broker_url.c_str(), mqtt_broker_port.toInt());
+    client.setCallback(mqtt_callback);
+   }    
+}
+
   
 void handleSave()
 {
@@ -213,11 +303,9 @@ void handleSave()
     for (uint8_t i = 0; i < server.args(); i++) {
         // SNY MODE = enable ntc sync
         if (server.argName(i) == "sync_mode") {
-            if (server.arg(i) == "sync_off") {
-                sync_mode = 0;
-            }
-            else if (server.arg(i) == "sync_on") {
-                sync_mode = 1;
+            sync_mode = server.arg(i).toInt();
+            if(sync_mode < 0 || sync_mode > 2){
+              sync_mode = 0;
             }
             last_error = "set sync_mode to" + String(sync_mode);
         }
@@ -240,6 +328,30 @@ void handleSave()
             timeClient.forceUpdate();
             last = 0;
         }
+
+        // mqtt_broker_url
+        if (server.argName(i) == "mqtt_broker_url") {
+            mqtt_broker_url = server.arg(i);
+            last_error = "set mqtt_broker_url to" + mqtt_broker_url;
+            last = 0;
+        }
+       
+
+        // mqtt_broker_port
+        if (server.argName(i) == "mqtt_broker_port") {
+            mqtt_broker_port = server.arg(i);
+            last_error = "set mqtt_broker_port to" + mqtt_broker_port;
+            last = 0;
+        }
+
+        // mqtt_broker_port
+        if (server.argName(i) == "mqtt_topic") {
+            mqtt_topic = server.arg(i);
+            last_error = "set mqtt_topic to" + mqtt_topic;
+            last = 0;
+        }
+
+        
         // formats the filesystem= resets all settings
         if (server.argName(i) == "fsformat") {
             if (SPIFFS.format()) {
@@ -256,9 +368,13 @@ void handleSave()
 
         //LOAD FACOTRY RESET
         if (server.argName(i) == "factreset") {
-            sync_mode = 1;
+           sync_mode = 1;
            timezone = DEFAULT_TIMEZONE;
            ntp_server_url = DEFAULT_NTP_SERVER;
+           mqtt_broker_url = DEFAULT_MQTT_BROKER;
+           mqtt_broker_port = DEFAULT_MQTT_BROKER_PORT;
+           mqtt_topic = DEFAULT_MQTT_TOPIC;
+           
            timeClient.forceUpdate();
         }
 
@@ -279,11 +395,19 @@ void handleSave()
     }
     //SAVE THESE DATA
     save_values_to_eeprom();
-
+    //SAVE MQTT STUFF 
+    setup_mqtt_client();
+    
     server.send(404, "text/html",
         "<html><head><meta http-equiv='refresh' content='1; url=/' "
         "/></head><body>SAVE SETTINGS PLEASE WAIT</body></html>");
 }
+
+
+
+
+
+
 void handleRoot()
 {
   String timezonesign = "";
@@ -296,46 +420,56 @@ void handleRoot()
 
 
     control_forms += "<hr><h2>CONTROLS</h2>";
-    control_forms += "<br><h3> MAIN CONTROLS </h3>";
+    control_forms += "<br><h3> MODE CONTROL </h3><br>";
 
 
-    if (sync_mode == 0) {
+    //if (sync_mode == 0) {
         control_forms += "<form name='btn_on' action='/save' method='GET' required >"
-                         "<input type='hidden' value='sync_on' name='sync_mode' />"
-                         "<input type='submit' value='ENABLE NTP SYNCHRONISIERUNG'/>"
+                         "<select name='sync_mode' id='sync_mode'>";
+
+                         if(sync_mode == 0){
+                            control_forms += "<option value='0' selected>DISABLED</option>";
+                         }else{
+                            control_forms += "<option value='0'>DISABLED</option>";
+                         }
+
+                         if(sync_mode == 1){
+                            control_forms += "<option value='1' selected>NTP-SYNC</option>";
+                         }else{
+                            control_forms += "<option value='1'>NTP-SYNC</option>";
+                         }
+
+                         if(sync_mode == 2){
+                            control_forms += "<option value='2' selected>MQTT-SYNC</option>";
+                         }else{
+                            control_forms += "<option value='2'>MQTT-SYNC</option>";
+                         }
+                         
+                     
+        control_forms += "</select>"
+                         "<input type='submit' value='SAVE'/>"
                          "</form>";
-    }
-    else {
-        control_forms += "<br>";
-        control_forms += "<form name='btn_off' action='/save' method='GET' required>"
-                         "<input type='hidden' value='sync_off' name='sync_mode' />"
-                         "<input type='submit' value='DISABLE NTP SYNCHRONISIERUN'/>"
-                         "</form>";
-    }
+    
+    
+
+
+     control_forms += "<br><h3> CLOCK CONTROLS </h3>";
+    
+      control_forms += "<form name='btn_off' action='/save' method='GET'>"
+                     "<input type='number' value='"+ String(timezone) + "' name='timezone' min='-12' max='12' required placeholder='1'/>"
+                     "<input type='submit' value='SET TIMEZONE'/>"
+                     "</form>";
 
     control_forms += "<br>";
     control_forms += "<br>";
     control_forms += "<br>";
     control_forms += "<form name='btn_off' action='/save' method='GET'>"
-                     "<input type='number' value='"
-        + String(timezone) + "' name='timezone' min='-12' max='12' required placeholder='1'/>"
-                             "<input type='submit' value='SET TIMEZONE'/>"
-                             "</form>";
-
-    control_forms += "<br>";
-    control_forms += "<br>";
-    control_forms += "<br>";
-    control_forms += "<form name='btn_off' action='/save' method='GET'>"
-                     "<input type='text' value='"
-        + ntp_server_url + "' name='ntp_server_url' required placeholder='pool.ntp.org'/>"
-                           "<input type='submit' value='SET NTP SERVER URL'/>"
-                           "</form>";
+                     "<input type='text' value='"+ ntp_server_url + "' name='ntp_server_url' required placeholder='pool.ntp.org'/>"
+                     "<input type='submit' value='SET NTP SERVER URL'/>"
+                     "</form>";
+                     
     control_forms += "<br>";
     
-     control_forms += "<form name='btn_on' action='/save' method='GET' required >"
-                     "<input type='hidden' value='abi' name='abi' />"
-                     "<input type='submit' value='START ANTI BURN IN CYCLE'/>"
-                     "</form>";
 
    control_forms += "<form name='btn_on' action='/save' method='GET' required >"
                      "<input type='hidden' value='sendtime' name='sendtime' />"
@@ -345,37 +479,69 @@ void handleRoot()
 
 
 
+  control_forms += "<br><h3> MQTT SETTINGS </h3>";
+  control_forms += "<form name='btn_off' action='/save' method='GET'>"
+                     "<input type='text' value='"+ String(mqtt_broker_url) + "' name='mqtt_broker_url' required placeholder='broker.hivemq.com'/>"
+                     "<input type='submit' value='SAVE MQTT BROKER'/>"
+                     "</form>";
+
+  control_forms += "<form name='btn_off' action='/save' method='GET'>"
+                     "<input type='number' value='"+ String(mqtt_broker_port) + "' name='mqtt_broker_port' min='1' max='65536' required placeholder='1883'/>"
+                     "<input type='submit' value='SET MQTT BROKER PORT'/>"
+                     "</form>";
+  control_forms += "<form name='btn_off' action='/save' method='GET'>"
+                     "<input type='text' value='"+ String(mqtt_topic) + "' name='mqtt_topic' required placeholder='/iot/sensor/temp'/>"
+                     "<input type='submit' value='SET MQTT TOPIC'/>"
+                     "</form>";
+   
+
 
      control_forms += "<br><h3> OTHER SETTINGS </h3>";
 
     control_forms += "<form name='btn_on' action='/save' method='GET' required >"
                      "<input type='hidden' value='eepromread' name='eepromread' />"
                      "<input type='submit' value='READ STORED CONFIG'/>"
-                     "</form>";
+                     "</form><br>";
 
     control_forms += "<form name='btn_on' action='/save' method='GET' required >"
                      "<input type='hidden' value='fsformat' name='fsformat' />"
                      "<input type='submit' value='DELETE CONFIGURATION'/>"
-                     "</form>";
-    
+                     "</form><br>";
+                     
+    control_forms += "<form name='btn_on' action='/save' method='GET' required >"
+                     "<input type='hidden' value='abi' name='abi' />"
+                     "<input type='submit' value='START ANTI BURN IN CYCLE'/>"
+                     "</form><br>";
+
+
+                     
      control_forms += "<form name='btn_on' action='/save' method='GET' required >"
                      "<input type='hidden' value='factreset' name='factreset' />"
                      "<input type='submit' value='FACTORY RESET'/>"
-                     "</form>";
+                     "</form><br>";
 
-    control_forms += "<br>";
-    control_forms += "<br><hr><h3>LAST ERROR</h3><br>" + last_error;
+
+
+    control_forms += "<br><hr><h3>LAST SYSTEM MESSAGE</h3><br>" + last_error;
 
 
     
 
     server.send(200, "text/html", phead_1 + WEBSITE_TITLE + phead_2 + pstart + control_forms + pend);
 }
+
+
+
 void handleNotFound()
 {
-    server.send(404, "text/html",
-        "<html><head>header('location: /'); </head></html>");
+    server.send(404, "text/html","<html><head>header('location: /'); </head></html>");
 }
+
+
+
+
+
+
 void setup(void)
 {
     Serial.begin(SERIAL_BAUD_RATE);
@@ -397,7 +563,7 @@ void setup(void)
     // START WFIFIMANAGER FOR CAPTIVE PORTAL
     WiFiManager wifiManager;
     wifiManager.setDebugOutput(false);
-    wifiManager.autoConnect("NixieClockConfiguration");
+    wifiManager.autoConnect("LixieClockConfiguration");
 
     if (MDNS.begin(MDNS_NAME)) {
     }
@@ -410,6 +576,7 @@ void setup(void)
     server.begin();
     
     //START OTA LIB
+    ArduinoOTA.setHostname(MDNS_NAME);
     ArduinoOTA.onStart([]() {
         String type;
         if (ArduinoOTA.getCommand() == U_FLASH) {
@@ -420,10 +587,8 @@ void setup(void)
         }
         SPIFFS.end();
     });
-    ArduinoOTA.onEnd([]() { Serial.println("\nEnd"); });
-    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-        Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-    });
+    ArduinoOTA.onEnd([]() { /*Serial.println("\nEnd");*/ });
+    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {/*Serial.printf("Progress: %u%%\r", (progress / (total / 100)));*/});
     ArduinoOTA.onError([](ota_error_t error) {
         // Serial.printf("Error[%u]: ", error);
         if (error == OTA_AUTH_ERROR) {
@@ -451,11 +616,13 @@ void setup(void)
     ArduinoOTA.begin();
 
 
-
+    //SETUP  NTP CLIENT
     timeClient.setTimeOffset(timezone);
     timeClient.setPoolServerName(ntp_server_url.c_str());
-
     timeClient.begin();
+
+    //SETUP MQTT
+    setup_mqtt_client();
 
 
 }
@@ -475,10 +642,22 @@ void loop(void)
     //SEND NTP TIME TO CLOCK
     if ((millis() - last) > 1000 * 60 * NTP_SEND_TIME_INTERVAL) {
         last = millis();   
-        if (sync_mode) {
+        if (sync_mode == 1) {
             send_time_to_clock();
         }
     }
+
+      
+     //MQTT RECONNECT FUNCTION
+    if (!client.connected()) {
+      mqtt_reconnect();
+    }
+    //MQTT LOOP FKT
+    if(sync_mode == 2){
+      client.loop();
+    }
+
+    
     //HANDLE OTA
     ArduinoOTA.handle();
     
